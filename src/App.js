02c68703cb551a80c751d88788b234974d625e30
Carlo <john.carlo9517@gmail.com>
2020-03-22 16:09:11 +0800

    Applied ProjectList component in App.js

--- src/App.js
+++ src/App.js
@@ -1,135 +1,14 @@
 import React from "react";
-import { Layout } from "antd";
-import styled from "@emotion/styled";
 
 import MainLayout from "./modules/MainLayout";
-
-const { Content: antContent } = Layout;
+import ProjectList from "./modules/ProjectList";
 
 function App() {
   return (
     <MainLayout>
-      <Content>
-        <Wrapper>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-          <div>CONTENT</div>
-        </Wrapper>
-      </Content>
+      <ProjectList />
     </MainLayout>
   );
 }
-
-// TODO: import these somewhere else
-const Content = styled(antContent)`
-  padding: 50px 50px 0px 50px;
-`;
-
-const Wrapper = styled.div`
-  background: #fff;
-  padding: 24px;
-  min-height: 600px;
-`;
 
 export default App;
