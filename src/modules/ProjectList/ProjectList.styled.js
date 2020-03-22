933f9161503bc5c0877dcb0c21a7c37dd213ee27
Carlo <john.carlo9517@gmail.com>
2020-03-22 16:08:45 +0800

    Created ProjectList component

---
+++ src/modules/ProjectList/ProjectList.styled.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+import { Layout } from "antd";
+
+const { Content: antContent } = Layout;
+
+export const Content = styled(antContent)`
+  padding: 50px 50px 0px 50px;
+`;
+
+export const Wrapper = styled.div`
+  background: #fff;
+  padding: 24px;
+  min-height: 600px;
+`;
